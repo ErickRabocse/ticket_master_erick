c84929b160c200068e0e0bfee917b1aaf8a2ac30
Erick Chavez <erickchavezescobar@gmail.com>
2024-04-30 22:31:14 -0600

    events and eventItem components now return data from events json

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const EventItem = () => {
+const EventItem = ({ img, name, info }) => {
   return (
     <div>
-      Event Item
+      <img src={img} alt='event image' style={{ height: '200px' }} />
+      <h4>{name}</h4>
+      <p>{info}</p>
     </div>
   )
 }
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EventItem from './components/EventItem'
-import data from '../../data/events.json'
+// import data from '../../data/events.json'
+import eventsJSON from '../../data/events.json'
 
 const Events = () => {
-  // console.log(data)
-  // const eventos = data._embedded
-  // console.log(eventos)
+  const [data] = useState(eventsJSON)
   const { _embedded: { events } } = data
   console.log(events)
-
+  const eventComponent = events.map((eventItem) => (
+    <EventItem
+      key={eventItem.id}
+      img={eventItem.images[1].url}
+      name={eventItem.name}
+      info={eventItem.promoter?.description}
+    />
+  ))
   return (
     <div>
       Events
-      <EventItem />
+      {eventComponent}
     </div>
   )
 }
