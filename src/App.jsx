eca5055b9720bced513c2846f48b607887d031bc
Erick Chavez <erickchavezescobar@gmail.com>
2024-05-03 22:51:17 -0600

    useRef and forward ref implemented to obtain the reference of a div element

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Events from './components/Events'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 function App () {
   const [searchTerm, setSearchTerm] = useState('')
+  const containerRef = useRef()
 
   const handleNavbarSearch = (term) => {
     setSearchTerm(term)
+    console.log(containerRef.current)
   }
   return (
     <>
-      <Navbar onSearch={handleNavbarSearch} />
+      <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
       <Events searchTerm={searchTerm} />
     </>
   )
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, forwardRef } from 'react'
 
-const Navbar = ({ onSearch }) => {
+const Navbar = forwardRef(({ onSearch }, ref) => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const Navbar = ({ onSearch }) => {
     }
   }
   return (
-    <div>
+    <div ref={ref}>
       <p>My tickets</p>
       <input
         placeholder='Busca tu evento favorito'
@@ -27,6 +27,8 @@ const Navbar = ({ onSearch }) => {
       />
     </div>
   )
-}
+})
+
+Navbar.displayName = 'Navbar'
 
 export default Navbar
