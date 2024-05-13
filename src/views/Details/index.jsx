85ac406396bc646001f4427fb34818ea4bb5b22e
Erick Chavez <erickchavezescobar@gmail.com>
2024-05-12 19:10:26 -0600

    useParams added to Details view to obtain the eventId declared in routes which shows the unique id of each event

--- src/views/Details/index.jsx
+++ src/views/Details/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Details = () => {
+  const { eventId } = useParams()
+  console.log(eventId)
   return (
     <div>
       Details view component
