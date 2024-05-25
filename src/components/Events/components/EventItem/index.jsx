9c1d8d4d7975b35d9e3b4d5b29031006ccfd6be0
Erick Chavez <erickchavezescobar@gmail.com>
2024-05-24 23:46:58 -0600

    Adding like/unlike buttons to the events

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import styles from './EventItem.module.css'
 // import { Link } from 'react-router-dom'
+import filledHeart from '../../../../assets/filled-heart.png'
+import emptyHeart from '../../../../assets/empty-heart.png'
 
 const EventItem = ({ img, name, info, onEventClick, id }) => {
   const handleSeeMoreClick = (e) => {
     e.stopPropagation()
     onEventClick(id)
   }
+
+  const handleHeartClick = () => {
+
+  }
+
   return (
     <div onClick={() => console.log('parent clicked')} className={styles.eventItemContainer}>
-      <img src={img} alt='event image' style={{ width: '200px' }} />
+      <div className={styles.imageContainer}>
+        <img src={emptyHeart} alt='filled heart icon' className={styles.filledHeart} onClick={handleHeartClick} />
+        <img src={img} alt='event image' style={{ width: '200px' }} />
+      </div>
       <div className={styles.eventInfoContainer}>
         <h4 className={styles.eventName}>{name}</h4>
         <p className={styles.eventInfo}>{info}</p>
--- src/state/events-results.js
+++ src/state/events-results.js
@@ -17,3 +17,5 @@ const useEventsResults = create((set) => ({
 }))
 
 export default useEventsResults
+
+
