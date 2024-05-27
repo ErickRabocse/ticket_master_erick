ff414fd1c2f0346a3a790dd917895337dfd03df6
Erick Chavez <erickchavezescobar@gmail.com>
2024-05-26 19:57:46 -0600

    Adding like/unlike buttons to the events

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -3,21 +3,24 @@ import styles from './EventItem.module.css'
 // import { Link } from 'react-router-dom'
 import filledHeart from '../../../../assets/filled-heart.png'
 import emptyHeart from '../../../../assets/empty-heart.png'
+import useLikeEvents from '../../../../hooks/useLikeEvents'
 
 const EventItem = ({ img, name, info, onEventClick, id }) => {
+  const { isEventLiked, toggleEventLike } = useLikeEvents(id)
+
   const handleSeeMoreClick = (e) => {
     e.stopPropagation()
     onEventClick(id)
   }
 
   const handleHeartClick = () => {
-
+    toggleEventLike()
   }
 
   return (
     <div onClick={() => console.log('parent clicked')} className={styles.eventItemContainer}>
       <div className={styles.imageContainer}>
-        <img src={emptyHeart} alt='filled heart icon' className={styles.filledHeart} onClick={handleHeartClick} />
+        <img src={isEventLiked ? filledHeart : emptyHeart} alt='filled heart icon' className={styles.filledHeart} onClick={handleHeartClick} />
         <img src={img} alt='event image' style={{ width: '200px' }} />
       </div>
       <div className={styles.eventInfoContainer}>
